9465615d245df7d00fb2449d22cccdca64803c10
Idan Elfasi <idan60167@gmail.com>
2024-06-11 17:15:27 +0300

    get req from the front and get res of the data by the app navigate

--- server.js
+++ server.js
@@ -1,4 +1,42 @@
 import express from 'express' 
+
+
+import { bugService } from './services/bug.service.js'
+
 const app = express() 
-app.get('/puki', (req, res) => res.send('Hello muki')) 
+
+
+
+app.get('/api/bug', (req, res) =>{
+    bugService.query()
+    .then(bugs=>res.send(bugs))
+})
+
+app.get('/api/bug/save', (req, res) =>{
+    const {_id,title,severity}=req.query
+    const bugToSave={_id:_id||'',title,severit:+severity,}
+
+    bugService.save(bugToSave)
+    .then(savedBug=>res.send(savedBug))
+})
+
+ app.get('/api/bug/:id', (req, res) =>{
+    const {id} = req.params
+
+    bugService.getById(id)
+    .then(bug => res.send(bug))
+
+}) 
+ app.get('/api/bug/:id/remove', (req, res) =>{
+    const {id} = req.params
+    bugService.remove(id)
+    .then(() => res.send(`Bug ${id} deleted...`))
+}) 
+app.get('/puki', (req, res) =>{
+
+    res.send(`Hello puki`)
+}) 
+
+
+
 app.listen(3030, () => console.log('Server ready at port http://127.0.0.1:3030')) 
